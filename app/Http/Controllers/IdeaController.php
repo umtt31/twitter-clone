a5a358523f77e6265f7d3641ca51f0b69cd8f9b6
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-04-29 23:47:44 +0300

    Validation for ideas field

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -9,6 +9,11 @@ class IdeaController extends Controller
 {
     //
     public function store() {
+
+        request()->validate([
+            'idea' => 'required|min:3|max:240'
+        ]);
+
         $idea = Idea::create(['content' => request()->get('idea', '')]);
 
         return redirect()->route('dashboard')->with('success','Idea created successfully!');
