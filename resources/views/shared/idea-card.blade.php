5d672cf13a25cf7f87caca5417261c18c6d3a49e
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-04 18:08:13 +0300

    CRUD Update

--- resources/views/shared/idea-card.blade.php
+++ resources/views/shared/idea-card.blade.php
@@ -13,6 +13,7 @@
                 <form action="{{route('ideas.destroy', $idea->id)}}" method="POST">
                     @csrf
                     @method('DELETE')
+                    <a class="mx-2" href="{{route('ideas.edit', $idea->id)}}"> Edit</a>
                     <a href="{{route('ideas.show', $idea->id)}}"> View </a>
                     <button class="ms-1 btn btn-danger btn-sm"> X </button>
                 </form>
@@ -20,9 +21,26 @@
         </div>
     </div>
     <div class="card-body">
-        <p class="fs-6 fw-light text-muted">
-            {{$idea['content']}}
-        </p>
+        @if ($editing ?? false)
+            <form action="{{ route('ideas.update', $idea->id)}}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <textarea name="content" class="form-control" id="content" rows="3">{{$idea->content}}</textarea>
+                    @error('content')
+                        <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
+                    @enderror
+                </div>
+                <div class="">
+                    <button type="submit" class="btn btn-dark"> Update </button>
+                </div>
+        </form>
+        @else
+            <p class="fs-6 fw-light text-muted">
+                {{$idea['content']}}
+            </p>
+        @endif
+
         <div class="d-flex justify-content-between">
             <div>
                 <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
