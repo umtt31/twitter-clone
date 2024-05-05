13cbdc59dc4f938f20eaa83c08d48c87ae8312b9
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-05 08:36:55 +0300

    email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,44 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeMail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail as FacadesMail;
+use Mail;
 
 class AuthController extends Controller
 {
     //
-    public function register() {
+    public function register()
+    {
         return view('auth.register');
     }
 
-    public function store() {
+    public function store()
+    {
         $validated = request()->validate([
             'name' => 'required|min:3|max:40',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|confirmed|min:8',
         ]);
 
-        User::create([
+        $user = User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
         ]);
 
+        Mail::to($user->email)->send(new WelcomeMail($user));
+
         return redirect()->route('dashboard')->with('success', 'Account created Successfully!');
     }
 
-    public function login() {
+    public function login()
+    {
         return view('auth.login');
     }
 
-    public function authenticate() {
+    public function authenticate()
+    {
         $validated = request()->validate([
             'email' => 'required|email',
             'password' => 'required|min:8',
         ]);
 
-        if (auth()->attempt($validated)){
+        if (auth()->attempt($validated)) {
             request()->session()->regenerate();
             return redirect()->route('dashboard')->with('success', 'Logged in successfully!');
         }
@@ -47,10 +56,10 @@ class AuthController extends Controller
         return redirect()->route('login')->withErrors([
             'email' => 'No matching user found with the provided email and password.'
         ]);
-
     }
 
-    public function logout() {
+    public function logout()
+    {
         auth()->logout();
         request()->session()->invalidate();
         request()->session()->regenerateToken();
