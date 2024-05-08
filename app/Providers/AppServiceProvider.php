4efb2e6907b024493dd97244cf5d2f96074b7934
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-08 12:05:22 +0300

    caching for topUsers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,7 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        $topUsers = Cache::remember('topUser', 60 * 2, function () {
+            return User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get();
+        });
 
-        View::share('topUsers', $topUsers = User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get());
+        View::share('topUsers', $topUsers);
     }
 }
